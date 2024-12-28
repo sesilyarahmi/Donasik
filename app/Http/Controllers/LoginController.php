e0b44078cfd808b20273e96d7f691b5730faa88d
alifiofajarnugraha <148094334+alifiofajarnugraha@users.noreply.github.com>
2024-12-28 16:15:25 +0700

    update login

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -10,7 +10,7 @@ class LoginController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index()    
     {
         //
     }

ae095e26cb5fcbfa37b972e6bcdb6bb0f7844bda
alifiofajarnugraha <148094334+alifiofajarnugraha@users.noreply.github.com>
2024-12-28 16:19:46 +0700

    update fitur

---
+++ app/Http/Controllers/ZakatController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Zakat;
+use Illuminate\Http\Request;
+
+class ZakatController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Zakat $zakat)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Zakat $zakat)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Zakat $zakat)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Zakat $zakat)
+    {
+        //
+    }
+}
